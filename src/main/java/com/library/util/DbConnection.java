c4be2b21dc9c83b280b67cf4a1cce6e955406f93
MARYAM12334 <maryamouhdan@gmail.com>
2024-12-18 02:41:56 +0100

    Commit 17

--- src/main/java/com/library/util/DbConnection.java
+++ src/main/java/com/library/util/DbConnection.java
@@ -5,11 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-
-    private static final String URL = System.getenv("DB_URL");
-    private static final String USER = System.getenv("DB_USER");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD");
-
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "22032001";
 
     public static Connection getConnection() throws SQLException {
         try {
